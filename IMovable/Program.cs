6559bfe8605a90fdec0c3e07e9f5f64f23198862
10952559 <102601502+10952559@users.noreply.github.com>
2024-06-17 10:22:30 +0000

    Add files via upload

---
+++ IMovable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+interface IMovable{
+    void Move();
+}
+
+class Car : IMovable
+{
+   public void Move(){
+    Console.WriteLine("The Car moves.");
+   } 
+}
+class Bicycle : IMovable
+{
+   public void Move(){
+    Console.WriteLine("The Bicycle moves.");
+   } 
+}
+
+class Program{
+    static void Main(String[] args){
+IMovable car = new Car();
+IMovable bicycle = new Bicycle();
+
+car.Move();
+bicycle.Move();
+    }
+}
